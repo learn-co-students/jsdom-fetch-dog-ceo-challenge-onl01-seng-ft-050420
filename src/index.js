3fb729f7381b39801f5200cbc816d733acdbb972
gd-m <gitanjalidmule@gmail.com>
2020-08-09 09:13:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+function fetchImages(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        renderImages(data)
+    })
+}
+
+function renderImages(data){
+    let imgDiv = document.querySelector("#dog-image-container") 
+    data.message.forEach(element => {
+    const img = document.createElement("img")
+        img.setAttribute("src", `${element}`)
+        img.style.height = "300px"
+        img.style.width = "300px"
+        imgDiv.appendChild(img)
+    });
+}
+
+function fetchBreeds(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+
+
+
+
+fetchImages(imgUrl);
+fetchBreeds(breedUrl);
+
