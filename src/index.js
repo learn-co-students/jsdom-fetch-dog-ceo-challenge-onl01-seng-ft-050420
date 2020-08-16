4f385fb7a204fef7c7ad63d815b795fa77bf3ec7
codewithirene567 <irenejinscott@gmail.com>
2020-08-15 22:19:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let dogBreeds = []
 
 function fetchImages(){
     return fetch(imgUrl)
@@ -10,78 +11,115 @@ function fetchImages(){
   }
 
   function renderImage(json) {
-    //console.log(json)  
+    
     const div = document.getElementById('dog-image-container')
       json.message.forEach(imgSource => {
-      const image = document.createElement('img') //create node element from each of the strings
+      const image = document.createElement('img') 
       image.src = imgSource
-       //json is object and json.message is going to show the array with 4
-       //console.log(image) //recommend using console.log
+    
        div.appendChild(image)
       
       })
     }
   
     function fetchBreeds(){
-        return fetch(imgUrl)
+        return fetch('https://dog.ceo/api/breeds/list/all')
         .then(resp => resp.json())
-        .then(breeds => renderBreeds(breeds));
+        .then(breeds => {
+            dogBreeds = Object.keys(breeds.message)
+            renderBreeds(dogBreeds)
+            changeColor()
+            filterBreeds()
+        });
+        
     }
 
     function renderBreeds(breeds){
-        //console.log(breeds.message) // this is returning an array of dog url image sources and their breeds are inside
-        const ul = document.getElementById('dog-breeds')
-        const li = document.createElement('li')
-        const li1 = document.createElement('li')
-        const li2 = document.createElement('li')
-        const li3 = document.createElement('li')
-        //console.log(li)
-        const breed = breeds.message[0].split("/")
-        const breed1 = breeds.message[1].split("/")
-        const breed2 = breeds.message[2].split("/")
-        const breed3 = breeds.message[3].split("/")
-        //function things(){
-        const thing = breed[4]
-        const thing1 = breed1[4]
-        const thing2 = breed2[4]
-        const thing3 = breed3[4]
-        //console.log(thing1)
-        //let allBreeds = function things()
-        //}
-        //const breed = breeds.message[0].split("/").slice(30)
-        //console.log(breed)
-        li.innerHTML = thing
-        li1.innerHTML = thing1
-        li2.innerHTML = thing2
-        li3.innerHTML = thing3
-        ul.appendChild(li)
-        ul.appendChild(li1)
-        ul.appendChild(li2)
-        ul.appendChild(li3)
+        
+         const ul = document.getElementById('dog-breeds')
+         breeds.forEach(breed => {
+             const li = document.createElement('li')
+             li.innerHTML = breed
+             ul.appendChild(li)
+          })
+        
+
+
     }
 
     function changeColor(){
         let li = document.querySelectorAll('li')
-        console.log(document.querySelectorAll('li'))
-      //if (li.addEventListener("click" === true)) {
-         colorChanged()
+        
+     
+         colorChanged(li)
       }
-      //}
-    
-      //function colorChanged(){
-      //  document.getElementById("li").style.color = "blue";
-      //}
 
-      function colorChanged(){
-          //document.getElementById("li").onclick = function(){
-            li.onclick = function(){
-             document.getElementById("li").style.color = "blue";
-        }
+      function colorChanged(lis){
+          
+        lis.forEach(li => {
+            li.onclick = function(e){ 
+                
+                e.target.style.color = "blue"; 
+           }
+        })
+            
     }
+
+function selectBreedStartingWithLetter(letter){
+        renderBreeds(breeds)
+}
+
+    function filterBreeds(){
+        let dropDown = document.getElementById("breed-dropdown")
+        let letters = document.querySelectorAll("option")
+        let filteredBreeds = []
+        dropDown.onclick = function(e){
+            let letter = e.target.value
+            
+            let breedsElements =  document.querySelectorAll('li')
+            
+            let breeds = []
+            
+         let allBreeds = document.getElementById('dog-breeds')
+         
+          allBreeds.innerHTML = "" //makes it all disappear
+         
+             breedsElements.forEach(element => {
+           
+               return breeds.push(element.innerHTML)
+               
+             })
+             console.log(breeds)
+              breeds.forEach(breed => {
+                 
+                 if (breed.split("")[0] === letter){
+                     
+                     let ul = document.querySelector('#dog-breeds')
+                     let li = document.createElement('li')
+                     
+                     li.innerHTML=breed
+                     
+                     
+                     //renderBreeds(breeds)
+                     
+                     ul.appendChild(li)
+                     
+                    
+                 }
+                 
+                 })
+              
+        }
+        
+       
+    }
+    
+       
+      
+    
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchImages() 
     fetchBreeds()
-    changeColor()
+    
 })
-  
